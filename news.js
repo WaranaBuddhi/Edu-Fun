968549cdb0754d44568add552b22f401b7f00d61
Buddhi Senarathna <senarathnabuddhi11@gmail.com>
2024-10-07 23:04:41 +0530

    Add files via upload

---
+++ news.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.read-more').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('This will take you to the full news article!');
+    });
+});
